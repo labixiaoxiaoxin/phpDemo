a4890c0e8cf1eb453456ad4311f2389b439105c6
labixiaoxiaoxin <920315622@qq.com>
2017-10-01 17:55:18 +0800

    日期时间函数
    日期时间函数

--- Datetest/jQuryTime.php
+++ Datetest/jQuryTime.php
@@ -5,6 +5,10 @@
 	<script type="text/javascript" src="/flipcountdown-master/jquery.min.js"></script>
 	<script type="text/javascript" src="/flipcountdown-master/jquery.flipcountdown.js"></script>
 	<link rel="stylesheet" type="text/css" href="/flipcountdown-master/jquery.flipcountdown.css" />
+
+	<link rel="stylesheet" type="text/css" href="/datetimepicker-master/jquery.datetimepicker.css"/ >
+	<!-- <script src="/datetimepicker-master/jquery.js"></script> -->
+	<script src="/datetimepicker-master/build/jquery.datetimepicker.full.min.js"></script>
 </head>
 <body>
 	<p>flipcountdown</p>
@@ -16,6 +20,12 @@
 		  	//pm:true
 		  });
 		})
+	</script>
+
+	<p>datetimepicker</p>
+	<input type="text" id="datetimepicker" name="">
+	<script type="text/javascript">
+		jQuery('#datetimepicker').datetimepicker();
 	</script>
 </body>
 </html>
